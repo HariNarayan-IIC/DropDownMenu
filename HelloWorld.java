7c8af90700fc75f2e09901dad8766726c5cb3244
R Hari Narayan <harinarayan2002@gmail.com>
2024-04-22 20:04:05 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("Hello World!!");
+    System.out.println("Hello World!!");
+    int a= 4;
+    int b= 6;
+    int sum= a+b;
+    System.out.print(sum);
+  }
+}
